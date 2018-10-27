72cb55f4d5500571175d753afa17bd71cef21c3b
Kaloyan Penov <s133967@student.dtu.dk>
2018-10-27 15:11:28 +0200

    - dev branch some change

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -2,7 +2,8 @@ public class AppMain {
 
     public static void main(String[] args){
         System.out.println("Hello from DiceGame2");
-        //this is dev
+        //this is DieClass branch
+        //some change to Die class branch
     }
 
 }
